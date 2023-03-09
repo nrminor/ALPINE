88f75aa00f5abe3b48bc75e3fadf28ab790eea68
Nick Minor <nrminor@wisc.edu>
2023-03-09 14:23:55 -0600

    adding code for finding the closest value to a prevalence quantile in the data

--- bin/compare_lineage_prevalences.R
+++ bin/compare_lineage_prevalences.R
@@ -39,6 +39,8 @@ clusterExport(cluster,
 pango_report$anachronistic <- do.call(c, 
                                       clusterApplyLB(cluster, 1:length(dates), function(i){
                                         
+                                        library(outbreakinfo)
+                                        
                                         # identifying all states in each HHS region. All
                                         # prevalences will be based on U.S. HHS region
                                         # rather than individual states. This is because
@@ -65,16 +67,19 @@ pango_report$anachronistic <- do.call(c,
                                           
                                         }
                                         
-                                        # Then, we get rid of zeroes, which will skew
-                                        # our methods below
-                                        prevalence_table <- prevalence_table[prevalence_table$proportion>0,]
+                                        # Then, we get rid of zeroes, which will
+                                        # skew our methods below. We also get
+                                        # rid of 1.0's, which are likely
+                                        # erroneous.
+                                        prevalence_table <- prevalence_table[prevalence_table$proportion>0 &
+                                                                               prevalence_table$proportion<1,]
                                         rownames(prevalence_table) <- NULL
                                         
                                         # Next, we use a rather crude method to identify
                                         # when the wave for a lineage in question has
                                         # "crested". This is the date of peak prevalence
                                         # for the lineage in the HHS region.
-                                        crest <- max(prevalence_table[
+                                        crest_date <- max(prevalence_table[
                                           prevalence_table$proportion==max(prevalence_table$proportion), 
                                           "date"])
                                         
@@ -91,15 +96,26 @@ pango_report$anachronistic <- do.call(c,
                                         # use the *last* date at which a lineage is in
                                         # its lowest 10% of prevalence in the HHS region
                                         # in question.
-                                        rare <- max(prevalence_table[
-                                          prevalence_table$proportion<=quantile(prevalence_table$proportion, 
-                                                                                0.1), "date"])
+                                        q <- quantile(prevalence_table$proportion, 0.01)
+                                        rare_prev <- prevalence_table$proportion[
+                                          which.min(
+                                            abs(prevalence_table$proportion - q))]
+                                        rare_date <- max(prevalence_table[
+                                          prevalence_table$proportion==rare_prev, "date"]) # + 30
                                         
                                         # Finally, an ifelse statement checks if the
                                         # collection date for a the lineage sample in
                                         # question comes after the above-computed "final date
                                         # of rarity"
-                                        ifelse(dates[i] > rare, return(TRUE), return(FALSE))
+                                        ifelse(dates[i] > rare_date, return(TRUE), return(FALSE))
                                         
                                       }))
 
+
+# plotting code for testing purposes
+# plot(prevalence_table$proportion~prevalence_table$date, pch = 20, col = "darkgray", xpd = T,
+#      xlab = "Collection Date", ylab = "Prevalence Estimate", main = "BA.5.2.1")
+# abline(v = crest_date)
+# text(crest_date + 2, 0.05, labels = "Peak\nPrevalence", adj = 0)
+# abline(v = rare_date)
+# text(rare_date + 2, 0.26, labels = "1% Quantile\nPrevalence", adj = 0)
