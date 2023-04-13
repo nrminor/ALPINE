415ace33e6c49b108bb55ca0fa29685f40fc03f7
Nicholas Minor <nrminor@wisc.edu>
2023-04-13 16:12:02 -0500

    adding a file lock so the script can write to disk right away instead of filling ram

--- bin/remove_fasta_gaps.py
+++ bin/remove_fasta_gaps.py
@@ -3,13 +3,17 @@
 import sys
 from Bio import SeqIO
 from Bio.Seq import Seq
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Lock
 
 # Define a function to process a single sequence record
-def process_seq_record(seq_record):
+def process_seq_record(seq_record, lock, out_handle):
 	seq_str = str(seq_record.seq).replace("-", "N")
 	seq_record.seq = Seq(seq_str)
-	return seq_record
+	
+	# Acquire the lock to write to the output file
+	lock.acquire()
+	SeqIO.write(seq_record, out_handle, "fasta")
+	lock.release()
 
 # Get input and output file names from command line arguments
 input_file = sys.argv[1]
@@ -23,7 +27,13 @@ with open(input_file, "r") as in_handle:
 	seq_records = list(SeqIO.parse(in_handle, "fasta"))
 
 # Process the sequence records in parallel
+lock = Lock()
 with open(output_file, "w") as out_handle:
 	with Pool(processes=num_processes) as pool:
-		for processed_seq_record in pool.imap(process_seq_record, seq_records):
-			SeqIO.write(processed_seq_record, out_handle, "fasta")
+		for seq_record in seq_records:
+			pool.apply_async(process_seq_record, (seq_record, lock, out_handle))
+
+		# Wait for all the processes to finish before closing the output file
+		pool.close()
+		pool.join()
+
