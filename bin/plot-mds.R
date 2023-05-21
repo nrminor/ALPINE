f64590a4485090d9eccfdef9feff0a2525a4527d
Nick Minor <nrminor@wisc.edu>
2023-05-21 13:45:40 -0500

    accounting for test dataset edge cases

--- bin/plot-mds.R
+++ bin/plot-mds.R
@@ -37,36 +37,60 @@ for (i in fasta_files){
   
 }
 
-# Calculate number of nucleotide differences
-dist_matrix <- ape::dist.dna(fasta_data, model = "N")
-
-# Perform MDS
-mds_result <- stats::cmdscale(dist_matrix) %>% 
-  as.data.frame()
-mds_result <- cbind(row.names(mds_result), mds_result) ; rownames(mds_result) <- NULL
-colnames(mds_result) <- c("Accession", "Dim1", "Dim2")
-
-# Read TSV table
-tsv_data <- readr::read_tsv(tsv_file,col_names=F)
-tsv_data <- tsv_data[tsv_data$X1=="S" | 
-                       tsv_data$X1=="H",]
-no_seqs <- nrow(tsv_data)
-colnames(tsv_data)[2] <- "Cluster"
-tsv_data$Cluster <- as.factor(tsv_data$Cluster)
-colnames(tsv_data)[9] <- "Accession"
-
-# Merge MDS results with TSV table
-merged_data <- merge(mds_result, tsv_data, by = "Accession", all.x = TRUE)
-
-# Plot MDS with colored points based on the table
-ggplot(merged_data, aes(x = Dim1, y = Dim2, color = Cluster)) +
-  geom_point() +
-  labs(title = paste("MDS Plot for", no_seqs, "sequences in", yearmonth, sep = " "), 
-       x = "Dimension 1", y = "Dimension 2")
-
-# Save the plot as a PDF
-ggsave(paste(yearmonth, "mds_plot.pdf", sep = "_"), width = 8.5, height = 10)
-
-# Save MDS results as a CSV
-write.csv(merged_data, paste(yearmonth, "mds_results.csv", sep = "_"),
-          quote = F, row.names = F)
+if (length(labels(fasta_data)) > 1){
+  
+  # Calculate number of nucleotide differences
+  dist_matrix <- ape::dist.dna(fasta_data, model = "N")
+  
+  # Perform MDS
+  mds_result <- stats::cmdscale(dist_matrix) %>% 
+    as.data.frame()
+  mds_result <- cbind(row.names(mds_result), mds_result) ; rownames(mds_result) <- NULL
+  colnames(mds_result) <- c("Accession", "Dim1", "Dim2")
+  
+  # Read TSV table
+  tsv_data <- readr::read_tsv(tsv_file,col_names=F)
+  tsv_data <- tsv_data[tsv_data$X1=="S" | 
+                         tsv_data$X1=="H",]
+  no_seqs <- nrow(tsv_data)
+  colnames(tsv_data)[2] <- "Cluster"
+  tsv_data$Cluster <- as.factor(tsv_data$Cluster)
+  colnames(tsv_data)[9] <- "Accession"
+  
+  # Merge MDS results with TSV table
+  merged_data <- merge(mds_result, tsv_data, by = "Accession", all.x = TRUE)
+  
+  # Plot MDS with colored points based on the table
+  ggplot(merged_data, aes(x = Dim1, y = Dim2, color = Cluster)) +
+    geom_point() +
+    labs(title = paste("MDS Plot for", no_seqs, "sequences in", yearmonth, sep = " "), 
+         x = "Dimension 1", y = "Dimension 2")
+  
+  # Save the plot as a PDF
+  ggsave(paste(yearmonth, "mds_plot.pdf", sep = "_"), width = 8.5, height = 10)
+  
+  # Save MDS results as a CSV
+  write.csv(merged_data, paste(yearmonth, "mds_results.csv", sep = "_"),
+            quote = F, row.names = F)
+  
+} else {
+  
+  # Read TSV table
+  tsv_data <- readr::read_tsv(tsv_file,col_names=F)
+  tsv_data <- tsv_data[tsv_data$X1=="S" | 
+                         tsv_data$X1=="H",]
+  no_seqs <- nrow(tsv_data)
+  colnames(tsv_data)[2] <- "Cluster"
+  tsv_data$Cluster <- as.factor(tsv_data$Cluster)
+  colnames(tsv_data)[9] <- "Accession"
+  
+  mds_failed <- data.frame(`MDS Failure Report` = 
+                             paste("Accession ", tsv_data$Accession, 
+                                   " is alone in its own cluster in ", yearmonth,
+                                   " and thus a distance matrix cannot be generated.", sep = ""))
+  
+  # Save MDS report as a CSV
+  write.csv(mds_failed, paste(yearmonth, "mds_failure_report.csv", sep = "_"),
+            quote = F, row.names = F)
+  
+}
