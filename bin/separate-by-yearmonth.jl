e4d3221b6302f4c715798638af5022b71785e56a
Nicholas Minor <nrminor@wisc.edu>
2023-08-01 11:08:23 -0500

    fixing small mistake in sep by month script

--- bin/separate-by-yearmonth.jl
+++ bin/separate-by-yearmonth.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 
 # loading necessary packages
-using LongInfectionFinder, FastaIO, FileIO, Dates, CSV, DataFrames, Arrow
+using LongInfectionFinder, FastaIO, FileIO, Dates, DataFrames, Arrow
 
 # Check if the input FASTA file is a symlink, and if it is, follow the symlink
 const fasta_path = islink(ARGS[1]) ? readlink(ARGS[1]) : ARGS[1]
@@ -10,7 +10,7 @@ const fasta_path = islink(ARGS[1]) ? readlink(ARGS[1]) : ARGS[1]
 const input_table_path = islink(ARGS[2]) ? readlink(ARGS[2]) : ARGS[2]
 
 # read in metadata
-metadata_df = DateFrame(Arrow.Table(input_table_path, DataFrame))
+metadata_df = DateFrame(Arrow.Table(input_table_path))
 
 # create lookup of dates and accessions
 accession_to_date = Dict(zip(metadata_df[!,"Accession"], metadata_df[!,"Isolate Collection date"]))
