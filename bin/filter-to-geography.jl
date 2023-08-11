16c18a52b882b918bd5b7e71bad597c3a6e13c39
Nicholas Minor <nrminor@wisc.edu>
2023-08-11 11:16:47 -0500

    trying more fixes to method selector

--- bin/filter-to-geography.jl
+++ bin/filter-to-geography.jl
@@ -4,10 +4,10 @@
 using LongInfectionFinder, DelimitedFiles, DataFrames, CSV, Dates, Pipe
 
 # saving command line arguments as static-typed constants
-const metadata_path = islink(ARGS[1]) ? readlink(ARGS[1]) : ARGS[1]
-const geo = ARGS[2]
-const max_date = ARGS[3]
-const min_date = ARGS[4]
+const METADATA_PATH::String = islink(ARGS[1]) ? readlink(ARGS[1]) : ARGS[1]
+const GEOGRAPHY::String = ARGS[2]
+const MAX_DATE::String = ARGS[3]
+const MIN_DATE::String = ARGS[4]
 
 # run the function based on the provided dates with multiple dispatch
 function main(metadata_path::String, geo::String, max_date::String, min_date::String)
@@ -23,7 +23,7 @@ function main(metadata_path::String, geo::String, max_date::String, min_date::St
 end
 
 # precompile main to eek out a bit more performance
-precompile(main, (String,String,String,String,));
+precompile(main, (String,String,String,String));
 
 # run main
-main(metadata_path, geo, max_date, min_date)
+main(METADATA_PATH, GEOGRAPHY, MAX_DATE, MIN_DATE)
