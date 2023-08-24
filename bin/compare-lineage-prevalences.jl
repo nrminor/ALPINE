6c44af7b5fab983a56ab43e1c019c257a6c79d1d
Nicholas Minor <nrminor@wisc.edu>
2023-08-24 11:29:59 -0500

    cleaning up a jl script

--- bin/compare-lineage-prevalences.jl
+++ bin/compare-lineage-prevalences.jl
@@ -1,27 +1,16 @@
 #!/usr/bin/env julia -t auto
 
-using LongInfectionFinder
+using LongInfectionFinder, RCall, CSV, DataFrames
 
 # define typed global constants based on command line arguments
 const REPORT_PATH::String = islink(ARGS[1]) ? readlink(ARGS[1]) : ARGS[1]
 const METADATA_PATH::String = islink(ARGS[2]) ? readlink(ARGS[2]) : ARGS[2]
 const FASTA_PATH::String = islink(ARGS[3]) ? readlink(ARGS[3]) : ARGS[3]
 
-# printing GISAID Disclaimer
-println("This data was obtained from GISAID via the outbreak.info API.")
-
-# function to pre-authenticate gisaid if run interactively
-# function authenticate_gisaid()
-
-#     R"authenticateUser()"
-
-# end
-# precompile(authenticate_gisaid)
-
 function main(report_path::String, metadata_path::String, fasta_path::String)
 
-    # gisaid authentication, if necessary
-    # authenticate_gisaid()
+    # printing GISAID Disclaimer; authenticate with R"authenticateUser()"
+    println("This data was obtained from GISAID via the outbreak.info API.")
 
     # filter the metadata
     new_metadata = assign_anachron(metadata_path, report_path)
