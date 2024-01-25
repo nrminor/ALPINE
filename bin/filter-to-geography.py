777669df8bb91551cd060e7bdbbd4705fb8abb45
Nicholas Minor <nrminor@wisc.edu>
2024-01-25 08:16:51 -0600

    adding possibility for two kinds of inclusion lists while filtering

--- bin/filter-to-geography.py
+++ bin/filter-to-geography.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-usage: filter-to-geography.py [-h] --metadata METADATA [--geography GEOGRAPHY] [--max_date MAX_DATE] [--min_date MIN_DATE]
+usage: filter-to-geography.py [-h] --metadata METADATA [--geography GEOGRAPHY] [--max_date MAX_DATE]
+                              [--min_date MIN_DATE] [--and_include AND_INCLUDE] [--or_include OR_INCLUDE]
 
 options:
   -h, --help            show this help message and exit
@@ -13,12 +14,17 @@ options:
                         The maximum date of interest. Defaults to today.
   --min_date MIN_DATE, -s MIN_DATE
                         The minimum date of interest. Defaults to None.
+  --and_include AND_INCLUDE, -a AND_INCLUDE
+                        Accessions to include among those filtered by date and geography
+  --or_include OR_INCLUDE, -o OR_INCLUDE
+                        Accessions to include in addition to those filtered by date and geography
 """
 
 import argparse
 import asyncio
 import os
 import sys
+from dataclasses import dataclass
 from datetime import date
 from enum import Enum
 from inspect import cleandoc
@@ -28,11 +34,11 @@ from typing import Optional
 import polars as pl
 from loguru import logger
 from pydantic import validate_call
-from pydantic.dataclasses import dataclass
+from pydantic.dataclasses import dataclass as pyd_dataclass
 from result import Err, Ok, Result
 
 
-@dataclass(frozen=True, kw_only=True)
+@pyd_dataclass(frozen=True, kw_only=True)
 class FilterParams:
     """
     The FilterParams dataclass contains the three relevant
@@ -45,6 +51,29 @@ class FilterParams:
     geography: str
     min_date: Optional[date]
     max_date: Optional[date]
+
+
+@pyd_dataclass(frozen=True, kw_only=True)
+class ExplicitInclusions:
+    """
+    The ExplicitInclusions dataclass contains two optional
+    paths to so-called "and inclusions" and "or inclusions,"
+    as specified in the command line.
+    """
+
+    and_inclusions: Optional[Path]
+    or_inclusions: Optional[Path]
+
+
+@dataclass
+class InclusionDFs:
+    """
+    The InclusionDFs dataclass contains two optional data
+    frames associated with any provided exclusion paths.
+    """
+
+    and_lf: Optional[pl.LazyFrame]
+    or_lf: Optional[pl.LazyFrame]
 
 
 class FileType(Enum):
@@ -119,6 +148,22 @@ def parse_command_line_args() -> Result[argparse.Namespace, str]:
         default=None,
         help="The minimum date of interest. Defaults to None.",
     )
+    parser.add_argument(
+        "--and_include",
+        "-a",
+        type=Path,
+        required=False,
+        default=None,
+        help="Accessions to include among those filtered by date and geography",
+    )
+    parser.add_argument(
+        "--or_include",
+        "-o",
+        type=Path,
+        required=False,
+        default=None,
+        help="Accessions to include in addition to those filtered by date and geography",
+    )
     args = parser.parse_args()
     if len(vars(args)) < 1:
         return Err("Zero arguments could be parsed.")
@@ -147,6 +192,50 @@ async def read_metadata(metadata_path: Path) -> Result[pl.LazyFrame, str]:
         Could not parse the input metadata file type. Please double check
         that input is CSV, TSV, or Apache Arrow/IPC files.
         """
+        )
+    )
+
+
+@logger.catch(reraise=True)
+async def read_inclusions(inclusions: ExplicitInclusions) -> Result[InclusionDFs, str]:
+    """
+    Read inclusion list handles, alongside any errors.
+    """
+
+    # initialize lazyframe option types
+    and_lf: Optional[pl.LazyFrame]
+    or_lf: Optional[pl.LazyFrame]
+
+    # check if inclusions lists are provided and scan them with Polars
+    if inclusions.and_inclusions is not None:
+        and_lf = pl.scan_csv(
+            inclusions.and_inclusions,
+            has_header=False,
+            separator="\t",
+            new_columns=["Accession"],
+        )
+    else:
+        and_lf = None
+    if inclusions.or_inclusions is not None:
+        or_lf = pl.scan_csv(
+            inclusions.or_inclusions,
+            has_header=False,
+            separator="\t",
+            new_columns=["Accession"],
+        )
+    else:
+        or_lf = None
+
+    # do some error handling
+    if and_lf is not None and and_lf.collect().shape[0] == 0:
+        return Err("And inclusion file that exists was specified, but it is empty.")
+    if or_lf is not None and or_lf.collect().shape[0] == 0:
+        return Err("OrAnd inclusion file that exists was specified, but it is empty.")
+
+    return Ok(
+        InclusionDFs(
+            and_lf=and_lf,
+            or_lf=or_lf,
         )
     )
 
@@ -201,6 +290,34 @@ async def filter_metadata(
     )
 
 
+async def handle_explicit_inclusions(
+    input_lf: pl.LazyFrame, meta_lf: pl.LazyFrame, includes: InclusionDFs
+) -> Result[pl.LazyFrame, str]:
+    """
+    `handle_explicit_inclusions()` does just that. It handles any provided
+    "and" and "or" inclusions lists by transforming the filtered metadata
+    to include those accessions. Note that this step does currently involve
+    mutating and executing the filtering query.
+    """
+
+    # start with any provided "and inclusions", where the user chooses to filter
+    # with the provided filter parameters and with a provided pre-curated accession list.
+    # Under these conditions, and accession must be on the include list *and* match
+    # the provided filters
+    if includes.and_lf is not None:
+        logger.info("Filtering metadata to provided 'and inclusions'.")
+        meta_lf = includes.and_lf.join(meta_lf, on="Accession", how="left")
+
+    # next, handle any "or inclusions", where the filtered metadata may contain any
+    # accessions that match the provided filters *or* be in the provided list
+    if includes.or_lf is not None:
+        logger.info("Filtering metadata to provided 'or inclusions'.")
+        or_df = includes.or_lf.join(input_lf, on="Accession", how="left").collect()
+        meta_lf = meta_lf.collect().vstack(or_df).lazy()
+
+    return Ok(meta_lf)
+
+
 @logger.opt(lazy=True).catch(reraise=True)
 async def write_out_accessions(filtered_lf: pl.LazyFrame) -> Result[None, str]:
     """
@@ -249,13 +366,29 @@ async def main() -> None:
         sys.exit("Command line argument parsing failed.")
     args = args_attempt.unwrap()  # pylint: disable=E1111
 
-    # unpack the individual argument components and re-pack filters
+    # unpack the metadata
     metadata_path = args.metadata
     assert os.path.isfile(metadata_path), "Provided metadata file does not exist."
+
+    # unpack any inclusion lists
+    if args.and_inclusions is not None:
+        assert os.path.isfile(
+            args.and_inclusions
+        ), "Provided and inclusion list file not found."
+    if args.or_inclusions is not None:
+        assert os.path.isfile(
+            args.or_inclusions
+        ), "Provided or inclusion list file not found."
+    inclusions = ExplicitInclusions(
+        and_inclusions=args.and_inclusions,
+        or_inclusions=args.or_inclusions,
+    )
+
+    # re-pack filter parameters provided in the command line
     filters = FilterParams(
         geography=args.geography,
-        min_date=None if args.min_date in ("null", "") else args.min_date,
-        max_date=date.today() if args.max_date in ("null", "") else args.max_date,
+        min_date=None if args.min_date in ("null", "", None) else args.min_date,
+        max_date=date.today() if args.max_date in (None, "null", "") else args.max_date,
     )
 
     # Scan the metadata into a LazyFrame and return any errors
@@ -263,6 +396,14 @@ async def main() -> None:
     metadata_attempt = await read_metadata(metadata_path)
     if isinstance(metadata_attempt, Err):
         sys.exit(metadata_attempt.unwrap_err())
+
+    # read any inclusion lists
+    logger.info("Reading inclusion lists, if provided.")
+    include_attempt = await read_inclusions(inclusions)
+    if isinstance(include_attempt, Err):
+        sys.exit(
+            f"Parsing of include files failed with the following error:\n{include_attempt.unwrap_err()}"
+        )
 
     # filter the metadata
     logger.info("Filtering metadata with provided filters.")
@@ -272,9 +413,18 @@ async def main() -> None:
             f"Metadata filtering failed with the following error:\n{lz_attempt.unwrap_err()}"
         )
 
+    # modify the polars query to handle inclusion lists
+    final_attempt = await handle_explicit_inclusions(
+        metadata_attempt.unwrap(), lz_attempt.unwrap(), include_attempt.unwrap()
+    )
+    if isinstance(final_attempt, Err):
+        sys.exit(
+            f"Transforming with include lists failed with the following error:\n{final_attempt.unwrap_err()}"
+        )
+
     # write out accessions
     logger.info("Writing out simple list of accessions as text file.")
-    acc_attempt = await write_out_accessions(lz_attempt.unwrap())
+    acc_attempt = await write_out_accessions(final_attempt.unwrap())
     if isinstance(acc_attempt, Err):
         sys.exit(f"Failed to separate out accessions with:\n{acc_attempt.unwrap_err()}")
 
